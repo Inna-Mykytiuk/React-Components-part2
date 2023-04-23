58ead2799bd98c9d9b8849195173c9e5d9612943
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-04-23 13:33:07 +0300

    update

--- src/components/index.js
+++ src/components/index.js
@@ -17,4 +17,3 @@ export * from 'components/SearchForm/SearchForm.styled';
 export * from 'components/Section/Section.styled';
 export * from 'components/Text/Text.styled';
 export * from 'components/Todo/Todo';
-// export * from 'components/Todo/Todo';
